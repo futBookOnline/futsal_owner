2660a6b8650045bc69dd0b41249c1de8d4e21bd1
BishalAryal1067 <bishalaryal1067@gmail.com>
2024-06-06 01:17:07 +0545

    Dashboard UI/ Overview Section Change

--- src/modules/dashboard/components/Overview.jsx
+++ src/modules/dashboard/components/Overview.jsx
@@ -3,18 +3,17 @@ import OverviewCard from './OverviewCard'
 
 const Overview = () => {
   return (
-    <>
-        <header >
-            <p className="text-xl">Overview</p>
-            <hr />
-        </header>
-        <div className='flex justify-between my-4'>
-            <OverviewCard title={"Total Bookings"} value={10} />
-            <OverviewCard title={"Remaining Slots"} value={4} />
-            <OverviewCard title={"Pending Bookings"} value={6} />
-            <OverviewCard title={"Total Earnings"} value={"3k"} />
-        </div>
-    </>
+    <div className='w-full'>
+      <header >
+        <p className="text-xl font-medium mb-2">Overview</p>
+      </header>
+      <div className='flex justify-start mt-3 gap-8'>
+        <OverviewCard title={"Total Bookings"} value={10} />
+        <OverviewCard title={"Remaining Slots"} value={4} />
+        <OverviewCard title={"Pending Bookings"} value={6} />
+        <OverviewCard title={"Total Earnings"} value={"3k"} />
+      </div>
+    </div>
   )
 }
 

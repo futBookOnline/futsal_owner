e0b5020bd54adab681f951207fb2584275413f45
unknown <sachinkhatri53@gmail.com>
2024-06-06 12:15:22 +0545

    Design clients page

---
+++ src/modules/dashboard/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div className="flex justify-center">
+      <input placeholder="Search" className="w-[50%] bg-slate-100 p-2 rounded-lg" />
+    </div>
+  );
+};
+
+export default Search;
